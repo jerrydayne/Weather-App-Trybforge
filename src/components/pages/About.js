6111d697e0911f2ab6af86e9e59834b480ba6e0a
Jerry <jerrydaexplorer@gmail.com>
2020-07-12 23:32:58 +0100

    welcome page and about. due to time

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Projects() {
     return (
-        <div>
-            About The author
+        <div className="salutation">
+            <h1>ABOUT ME</h1>
+            <p>lol...! there's  nothing to know about me for now</p>
+            <p>I just want to learn and do better.</p>
+            <p>This was suppose to be a nice about page</p>
+            <h1>IT WILL STILL BE DONE</h1>
         </div>
     )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 const  Home = () => {
+
+
     return (
-        <div>
-            Hompage
+        <div className="salutation">
+            <h1>welcome...</h1>
+            <p>this page was suppose to use your 
+                geo-location coordinates and display the weather conditions
+                of your current location...
+            </p>
+            <h1>WE ARE WORKING ON IT</h1>
+            <p>Please use the "weather finder" on the menu to check for weather conditions of a city..</p>
         </div>
     )
 }
